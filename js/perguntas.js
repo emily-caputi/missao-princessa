8d9c5dea9569373c396b6a5c2313a9c1bee6e7ac
Emily <132068134+emily-caputi@users.noreply.github.com>
2025-06-23 09:30:33 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas= [
+    {
+        enunciado: "No dia do seu 16º aniversário, ao invés de um presente comum, seu pai, o Rei, apresenta a você um espelho mágico que pode mostrar qualquer coisa no reino, inclusive responder a todas as suas perguntas com sabedoria ancestral. Qual é o seu primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início, você ficou apreensiva com o poder do espelho, temendo segredos sombrios.",
+                    "Achou assustador pensar no quão intrusiva essa magia poderia ser na vida das pessoas."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Você ficou encantada com a possibilidade de usar o espelho para aprender e ajudar seu povo.",
+                    "Pensou que a magia do espelho poderia resolver muitos dos problemas do reino."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar magia pode ser aterrorizante mesmo, e foi pensando nisso que sua tutora real decidiu que era hora de você aprender a governar de forma mais eficaz. Ela pede que você use o espelho para investigar um problema que afeta o vilarejo mais distante. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Você pede ao espelho para mostrar todos os detalhes sobre a colheita escassa e as necessidades do povo, buscando soluções diretamente.",
+                afirmacao: [
+                    "Você utilizou o espelho para obter informações cruciais e encontrar soluções práticas para o problema do vilarejo.",
+                    "Percebeu que o espelho pode ajudar a encontrar informações úteis sobre o reino de forma mais rápida e direcionada.",
+                    "Percebeu que o espelho consegue explicar as tradições e costumes complicados de forma simplificada, o que ajudou muito suas pesquisas sobre a cultura do reino."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Você decide visitar o vilarejo pessoalmente, conversando com os camponeses e entendendo suas dificuldades de perto.",
+                afirmacao: [
+                    "Você sentiu que a conexão pessoal era mais importante e confiou em sua própria observação para compreender a situação.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando meios mais tradicionais, como conversar com as pessoas, mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais segredos pessoais o espelho poderia revelar e por isso prefere resolver as coisas com pouca intromissão da magia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Um espelho mágico pode ser uma boa alternativa para realizar tarefas do dia a dia da realeza, e foi pensando nisso que sua tutora real decidiu que era hora de você aprender a governar de forma mais eficaz. Ela pede que você use o espelho para investigar um problema que afeta o vilarejo mais distante. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Você pede ao espelho para mostrar todos os detalhes sobre a colheita escassa e as necessidades do povo, buscando soluções diretamente.",
+                afirmacao: [
+                    "Você utilizou o espelho para obter informações cruciais e encontrar soluções práticas para o problema do vilarejo.",
+                    "Percebeu que o espelho pode ajudar a encontrar informações úteis sobre o reino de forma mais rápida e direcionada.",
+                    "Percebeu que o espelho consegue explicar as tradições e costumes complicados de forma simplificada, o que ajudou muito suas pesquisas sobre a cultura do reino."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Você decide visitar o vilarejo pessoalmente, conversando com os camponeses e entendendo suas dificuldades de perto.",
+                afirmacao: [
+                    "Você sentiu que a conexão pessoal era mais importante e confiou em sua própria observação para compreender a situação.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando meios mais tradicionais, como conversar com as pessoas, mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais segredos pessoais o espelho poderia revelar e por isso prefere resolver as coisas com pouca intromissão da magia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Artefatos mágicos podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um conselho real sobre como a magia impacta o futuro do reino. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Você defende a ideia de que a magia pode criar novas oportunidades para o reino e melhorar as habilidades do povo.",
+                afirmacao: [
+                    "Você se tornou uma defensora da inovação mágica, trabalhando para que ela beneficie a todos no reino, criando novas possibilidades e profissões.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos usando a magia."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Você expressa preocupação com o impacto da magia nas tradições e nos empregos dos artesãos, defendendo a importância de proteger as habilidades manuais do povo.",
+                afirmacao: [
+                    "Sua preocupação com o povo a motivou a criar escolas de ofícios, garantindo que as habilidades tradicionais não fossem perdidas com o avanço da magia.",
+                    "Criou grupos de ética voltados para a magia e busca ativamente reduzir as desigualdades geradas pela automação mágica."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um conselho real sobre como a magia impacta o futuro do reino. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Você defende a ideia de que a magia pode criar novas oportunidades para o reino e melhorar as habilidades do povo.",
+                afirmacao: [
+                    "Você se tornou uma defensora da inovação mágica, trabalhando para que ela beneficie a todos no reino, criando novas possibilidades e profissões.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos usando a magia."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Você expressa preocupação com o impacto da magia nas tradições e nos empregos dos artesãos, defendendo a importância de proteger as habilidades manuais do povo.",
+                afirmacao: [
+                    "Sua preocupação com o povo a motivou a criar escolas de ofícios, garantindo que as habilidades tradicionais não fossem perdidas com o avanço da magia.",
+                    "Criou grupos de ética voltados para a magia e busca ativamente reduzir as desigualdades geradas pela automação mágica."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas magias trazem novos desafios. Por isso, após a reunião do conselho, o Rei realizou um debate para entender as diferentes visões sobre o uso da magia. Nessa conversa, também foi levantado um ponto muito importante: como a magia impacta a vida e o trabalho do futuro no reino. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Você defende a ideia de que a magia pode criar novas oportunidades de emprego e melhorar as habilidades humanas dos artesãos e camponeses.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de magia e luta para abrir novos caminhos profissionais com ela.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos usando a magia para o bem do reino."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Você se preocupa com as pessoas que perderão seus empregos para máquinas e feitiços, defendendo a importância de proteger os trabalhadores do reino.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre os trabalhadores para discutir meios de utilização da magia de forma ética.",
+                    "Criou grupos de ética voltados para a magia e busca ativamente reduzir as desigualdades geradas pela automação mágica."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso, o Rei realizou um debate para entender como a magia impacta a vida e o trabalho no reino. Nessa conversa, também foi levantado um ponto muito importante: como a magia impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Você defende a ideia de que a magia pode criar novas oportunidades de emprego e melhorar as habilidades humanas dos artesãos e camponeses.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de magia e luta para abrir novos caminhos profissionais com ela.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos usando a magia para o bem do reino."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Você se preocupa com as pessoas que perderão seus empregos para máquinas e feitiços, defendendo a importância de proteger os trabalhadores do reino.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre os trabalhadores para discutir meios de utilização da magia de forma ética.",
+                    "Criou grupos de ética voltados para a magia e busca ativamente reduzir as desigualdades geradas pela automação mágica."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar um novo brasão para o reino que representasse o que pensa sobre o futuro da magia. E agora?",
+        alternativas: [
+            {
+                texto: "Você desenha o brasão à mão, com detalhes e cores que representam a história e os valores da sua família real, dedicando tempo e esmero a cada traço.",
+                afirmacao: [
+                    "Notou também que muitas pessoas no reino valorizavam as artes tradicionais e decidiu compartilhar seus conhecimentos de design, promovendo oficinas de pintura e tapeçaria.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Você utiliza o espelho mágico para projetar diversas opções de brasões, escolhendo o que melhor se adapta à sua visão e pedindo ao espelho que o materialize.",
+                afirmacao: [
+                    "Acelerou o processo de criação de novos símbolos e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também a magia para expressar suas ideias!",
+                    "Compartilhou artes mágicas em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a magia é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
